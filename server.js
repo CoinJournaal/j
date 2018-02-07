8dc5661167fbd8751e15b9296810bc8c2d5c799f
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-02-07 17:33:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express"),
     app = express();
 var fs = require("fs");
+const translate = require('google-translate-api');
 
 var port = process.env.PORT || 5000;
 
@@ -15,3 +16,16 @@ app.get("/sayHello", function (request, response) {
 
 app.listen(port);
 console.log("Listening on port ", port);
+
+translate('I spea Dutch!', {from: 'en', to: 'nl'}).then(res => {
+    console.log(res.text);
+    //=> Ik spreek Nederlands!
+    console.log(res.from.text.autoCorrected);
+    //=> true
+    console.log(res.from.text.value);
+    //=> I [speak] Dutch!
+    console.log(res.from.text.didYouMean);
+    //=> false
+}).catch(err => {
+    console.error(err);
+});
