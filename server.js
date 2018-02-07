c86ebec84e27ee0fc5ba6e75c96661a2e329f861
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-02-07 17:28:52 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express"),
+    app = express();
+var fs = require("fs");
+
+var port = process.env.PORT || 5000;
+
+global.res1;
+
+app.use(express.static(__dirname + '/public'));
+
+app.get("/sayHello", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.listen(port);
+console.log("Listening on port ", port);
