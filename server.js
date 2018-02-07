c77feebb2f0c421ba1b3d1dda7767d94491ba09b
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-02-07 21:45:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require("express"),
     app = express();
 var fs = require("fs");
 const translate = require('google-translate-api');
+var steem = require('steem');
 
 var port = process.env.PORT || 5000;
 
@@ -11,6 +12,7 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
+    postArticle(request.query.postingKey,'Auto-generated Steemit Post Test','auto-generated-steemit-post-test','Dit is een test. Excuus voor de spam.','test','api');
   response.end("Hello " + user_name + "!");
 });
 
@@ -29,3 +31,24 @@ translate('This is a test', {from: 'en', to: 'nl'}).then(res => {
 }).catch(err => {
     console.error(err);
 });
+
+function postArticle(postingKey, title, permlink, article, tag1, tag2)
+{
+  steem.broadcast.comment(
+    postingKey, // posting wif
+    '', // author, leave blank for new post
+    tag1, // first tag
+    'CoinJournaal', // username
+    permlink, // permlink
+    title, // Title
+    article, // Body of post
+    // json metadata (additional tags, app name, etc)
+    { tags: [tag2], app: 'steemjs' },
+    function (err, result) {
+      if (err)
+        alert('Failure! ' + err);
+      else
+        alert('Success!');
+    }
+  );
+}
