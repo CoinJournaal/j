6b5613811055aeef33652fb7b0a61a1bf2bb4d1e
CoinJournaal <34217520+CoinJournaal@users.noreply.github.com>
2018-02-08 14:15:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express"),
 var fs = require("fs");
 const translate = require('google-translate-api');
 var steem = require('steem');
+var http = require('request');
 
 var port = process.env.PORT || 5000;
 
@@ -12,25 +13,46 @@ app.use(express.static(__dirname + '/public'));
 
 app.get("/sayHello", function (request, response) {
   var user_name = request.query.user_name;
-    postArticle(request.query.postingKey,'Auto-generated Steemit Post Test','auto-generated-steemit-post-test','Dit is een test. Excuus voor de spam.','test','api');
+  //postArticle(request.query.postingKey,'Auto-generated Steemit Post Test','auto-generated-steemit-post-test','Dit is een test. Excuus voor de spam.','test','api');
   response.end("Hello " + user_name + "!");
+});
+
+app.get("/tl", function (request, response) {
+    translate('This is a test', {from: 'en', to: 'nl'}).then(res => {
+        console.log(res.text);
+        //=> Ik spreek Nederlands!
+        console.log(res.from.text.autoCorrected);
+        //=> true
+        console.log(res.from.text.value);
+        //=> I [speak] Dutch!
+        console.log(res.from.text.didYouMean);
+        //=> false
+    }).catch(err => {
+        console.error(err);
+    });
+});
+
+app.get("/cp", function (request, response) {
+    var url = 'https://www.cryptopanic.com/api/posts/?auth_token=31dc4817cba448911e694b5c2437c5215c6fbaaf&filter=bullish';
+
+    http.get(url, function(res){
+        var body = '';
+
+        res.on('data', function(chunk){
+            body += chunk;
+        });
+
+        res.on('end', function(){
+            var cpResponse = JSON.parse(body);
+            console.log("Got a response: ", cpResponse.count);
+        });
+    }).on('error', function(e){
+          console.log("Got an error: ", e);
+    });
 });
 
 app.listen(port);
 console.log("Listening on port ", port);
-
-translate('This is a test', {from: 'en', to: 'nl'}).then(res => {
-    console.log(res.text);
-    //=> Ik spreek Nederlands!
-    console.log(res.from.text.autoCorrected);
-    //=> true
-    console.log(res.from.text.value);
-    //=> I [speak] Dutch!
-    console.log(res.from.text.didYouMean);
-    //=> false
-}).catch(err => {
-    console.error(err);
-});
 
 var postArticle = function (postingKey, title, permlink, article, tag1, tag2)
 {
